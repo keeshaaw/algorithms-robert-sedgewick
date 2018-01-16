3d230b7d7caececb09356376a15de9faef9a3e4b
Kumar <Keshaw.Kumar@ad.agi>
2018-01-16 15:06:35 +0530

    Implementation of Uninon Find : Quick Union

--- src/main/java/lecture1/union/find/QuickFindUF.java
+++ src/main/java/lecture1/union/find/QuickFindUF.java
@@ -1,5 +1,29 @@
 package lecture1.union.find;
 
 public class QuickFindUF {
+	
+	private int ids[];
+	
+	public QuickFindUF(int N) {
+		ids = new int[N];
+		for(int i = 0; i < N; i++) {
+			ids[i] = i;
+		}
+	}
+	
+	public boolean find(int p, int q) {
+		return ids[p] == ids[q];
+	}
+	
+	public void union(int p, int q) {
+		int idP = ids[p];
+		int idQ = ids[q];
+		for(int i = 0; i < ids.length; i++) {
+			if(ids[i] == idP) ids[i] = idQ;
+		}
+		for(int i = 0; i < ids.length; i++) {
+			System.out.print(ids[i] + "\t");
+		}
+	}
 
 }
--- src/main/java/lecture1/union/find/QuickFindUFTest.java
+++ src/main/java/lecture1/union/find/QuickFindUFTest.java
@@ -1,8 +1,24 @@
 package lecture1.union.find;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class QuickFindUFTest {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		List<String> lines = Files.readAllLines(Paths.get(QuickFindUFTest.class.getResource("/TinyUF.txt").toURI()));
+		QuickFindUF uf = new QuickFindUF(Integer.parseInt(lines.get(0).trim()));
+		for(int i = 1; i < lines.size(); i++) {
+			String[] pq = lines.get(i).split(" ");
+			int p = Integer.parseInt(pq[0].trim());
+			int q = Integer.parseInt(pq[1].trim());
+			if(!uf.find(p, q)) {
+				uf.union(p, q);
+				System.out.println("Union of " + p + " and " + q);
+			}
+		}
 	}
 }
