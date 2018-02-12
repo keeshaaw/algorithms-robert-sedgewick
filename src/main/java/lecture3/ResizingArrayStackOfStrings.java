0b06a320ffb3597bc04416d2fe8de5ab4b060b5c
Keshaw Kumar <keeshaaw@gmail.com>
2018-02-12 23:34:14 +0530

    Array must be halved exacty when the size of the stack become on fourth of the array

--- src/main/java/lecture3/ResizingArrayStackOfStrings.java
+++ src/main/java/lecture3/ResizingArrayStackOfStrings.java
@@ -31,7 +31,7 @@ public class ResizingArrayStackOfStrings {
 	public String pop() {
 		String item = s[--N];
 		s[N] = null;
-		if(N > 0 && N <= s.length/4) {
+		if(N > 0 && N == s.length/4) {
 			resize(s.length/2);
 		}
 		return item;
